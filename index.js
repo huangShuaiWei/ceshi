4693ba454ddcca77da9c12a510a24af55898d059
hsw <2598196011@qq.com>
2024-04-15 15:38:11 +0800

    1

--- index.js
+++ index.js
@@ -46,7 +46,7 @@
       
           const a1 = document.createElement('a');
           a1.setAttribute('download', item.download);
-          a1.setAttribute('href', item.href);
+          a1.setAttribute('href', item.download1);
       
           const img = document.createElement('img');
           img.setAttribute('src', item.src);
