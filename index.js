9a1d2eae1e93e68b385ec00a86eeec0bb5bc8f01
hsw <2598196011@qq.com>
2024-04-15 14:15:18 +0800

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+    // ==UserScript==
+    // @name         视频
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  try to take over the world!
+    // @author       You
+    // @match        http://*user.hnzjpx.net/user/trainingclass*
+    // @grant        none
+    // ==/UserScript==
+
+
