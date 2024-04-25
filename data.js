5d8bdbb4cce76c8cb72d66c9410d9691ec717032
hsw <2598196011@qq.com>
2024-04-25 17:00:34 +0800

    搜索

---
+++ data.js
@@ -0,0 +1,6 @@
+// 类型 gerengboke 个人博客 
+
+const data = [
+    {type: 'gerengboke',name:"个人博客模板-1页",download: './zip/个人博客模板-1页.rar',src: './img/个人博客模板-1页.png', href: './img/个人博客模板-1页.png'},
+
+  ];
--- index.js
+++ index.js
@@ -9,16 +9,7 @@
     // ==/UserScript==
 
 
-    const data = [
-        {
-          type: 'type1',
-          name:"个人博客模板-1页",
-          download: './zip/个人博客模板-1页.rar',
-          src: './img/个人博客模板-1页.png',
-          href: './img/个人博客模板-1页.png',
-        },
-        // ... 其他数据
-      ];
+   
       
       const search = document.getElementById('search');
       const searchBtn = document.getElementById('searchBtn');
